9ef6510c082283cbfdb3463c5359c3a09b2d4798
Daniel Blazek <blazek.daniel@gmail.com>
2015-09-03 15:52:38 +0200

    Update snmpsitelist.sh

--- snmpsitelist.sh
+++ snmpsitelist.sh
@@ -2,48 +2,54 @@
 fileName=$1
 regexEntity="[a-zA-Z]*-[a-zA-Z0-9]*-[0-9]*e[0-9]\{3\}"
 
-touch temporaryFile temporaryFile2 validEntities invalidEntities
-touch validEntitiesNewFormat invalidEntitiesNewFormat
-rm temporaryFile temporaryFile2 validEntities invalidEntities
-rm validEntitiesNewFormat invalidEntitiesNewFormat
-
 if [ "$fileName" = "" ]; then
         echo "You not specify the filename!"
 elif [ "$fileName" != "" ]; then
 	read -p "Press [Enter] key to open file..."
-	vi $fileName
-	cat $fileName | awk {'print $1'} > temporaryFile 
-	echo "I am working..."
-	echo ""
-	while read line 
-	do 
-		site $line | grep $regexEntity | awk {'print $1, $2, $3;exit;'} >> temporaryFile2
-	done < temporaryFile
+    vi $fileName
+    cat $fileName | awk {'print $1'} > temporaryFile
+    echo "I am working..."
+        
+	while read line; do
+		if [ -e "temporaryFile2" ]; then rm temporaryFile2; fi
+		if [ -e "existornot" ]; then rm existornot; fi
+		site $line | grep $regexEntity | awk {'print $1, $2, $3'} >> temporaryFile2
+		grep $regexEntity temporaryFile2 >> existornot
+		[ -s existornot ] && echo $line "is valid" || echo $line "does not exist!" 
 			
-	#make two files, one will contains valid entity 
-	#second will contains invalid entity
-	while read line 
-	do 
-		typeOfDevice=$(echo $line | awk {'print $3'})
-		if [ "$typeOfDevice" == "Router" ]; then
-			echo $line >> validEntities
-		elif [ "$typeOfDevice" == "Switch" ]; then
-			echo $line >> validEntities
-		else
-			echo $line >> invalidEntities
-		fi
-	done < temporaryFile2
+		while read line2; do
+			if [[ $line2 == *"Router"* && $line2 == *W[0-1]*[a-zA-Z]*[0-9]* || $line2 == *"Switch"* && $line2 == *W[0-1]*[a-zA-Z]*[0-9]* ]]; then
+				echo $line2 >> validE
+				if [ -e "invalidE" ]; then rm invalidE; fi
+				break;
+			else 
+				echo $line2 >> invalidE 
+			fi
+		done < temporaryFile2
 	
-	awk '{ printf "%-30s %-10s %s\n", $1, $2, $3 }' validEntities >> validEntitiesNewFormat
-	awk '{ printf "%-30s %-10s %s\n", $1, $2, $3 }' invalidEntities >> invalidEntitiesNewFormat
-	
+	if [ -e "invalidE" ]; then
+		head -1 invalidE >> invalidE2
+		rm  invalidE
+	fi	
+	done < temporaryFile
+
+	awk '{ printf "%-37s %-10s %s\n", $1, $2, $3 }' validE >> validEntitiesNewFormat
+	awk '{ printf "%-37s %-10s %s\n", $1, $2, $3 }' invalidE2 >> invalidEntitiesNewFormat
+
 	#Final output
+	echo ""
 	echo "*** routers or switches ***"
 	cat validEntitiesNewFormat
 	echo ""
 	echo "*** other entities ***"
 	cat invalidEntitiesNewFormat
 	rm $fileName
-	rm temporaryFile temporaryFile2 validEntities invalidEntities
-	rm validEntitiesNewFormat invalidEntitiesNewFormat
+		
+	if [ -e "temporaryFile" ]; then rm temporaryFile; fi
+	if [ -e "temporaryFile2" ]; then rm temporaryFile2; fi
+	if [ -e "validEntitiesNewFormat" ]; then rm validEntitiesNewFormat; fi
+	if [ -e "invalidEntitiesNewFormat" ]; then rm invalidEntitiesNewFormat; fi
+	if [ -e "validE" ]; then rm validE; fi
+	if [ -e "invalidE2" ]; then rm invalidE2; fi
+	if [ -e "existornot" ]; then rm existornot; fi
 fi
